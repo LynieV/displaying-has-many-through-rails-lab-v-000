6b12aca914789e4bdde1a4eba794bd8ce5b707fd
Lynie McClimon <vmcclimon33@yahoo.com>
2020-02-27 01:42:27 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,6 @@
 class DoctorsController < ApplicationController
 
-  before_action :find_doctor, only: [:show, :edit]
+  before_action :find_doctor, only: [:show, :edit, :update, :destroy]
 
   def index
     @doctors = Doctor.all
